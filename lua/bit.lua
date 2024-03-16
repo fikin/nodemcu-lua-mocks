86e86df7d04ab790edbd871c30f2fdc8a755d3a8
Nikolay Fiykov <nikolai.fiikov@gmail.com>
2024-03-16 17:24:42 +0200

    adding more stock api for bit package.

--- lua/bit.lua
+++ lua/bit.lua
@@ -33,4 +33,62 @@ bit.bxor = function(a, b)
     return bit32.bxor(a, b)
 end
 
+---stock api
+---@param a number
+---@param b number
+---@return number
+bit.band = function(a, b)
+    return bit32.band(a, b)
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return number
+bit.rshift = function(a, b)
+    return bit32.rshift(a, b)
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return number
+bit.lshift = function(a, b)
+    return bit32.lshift(a, b)
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return boolean
+bit.isset = function(a, b)
+    return bit32.band(bit32.rshift(a, b), 1) == 1
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return boolean
+bit.isclear = function(a, b)
+    return bit32.band(bit32.rshift(a, b), 1) == 0
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return number
+bit.clear = function(a, b)
+    local mask = bit32.bnot(bit32.lshift(1, b))
+    return bit32.band(a, mask)
+end
+
+---stock api
+---@param a number
+---@param b number
+---@return number
+bit.set = function(a, b)
+    local mask = bit32.lshift(1, b)
+    return bit32.bor(a, mask)
+end
+
 return bit
